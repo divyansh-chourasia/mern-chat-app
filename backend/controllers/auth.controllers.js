75bb8fa5cfb2a9019fff4cd466643ac350e9d336
Divyansh Chourasia <Divyansh Chourasia@LAPTOP-ACKKCLSS.local>
2025-04-21 22:15:07 +0530

    authentication completed

--- backend/controllers/auth.controllers.js
+++ backend/controllers/auth.controllers.js
@@ -1,5 +1,6 @@
 import User from "../models/user.model.js";
 import bcrypt from "bcryptjs";
+import generateTokenAndSetCookie from "../utilis/generateToken.js";
 
 export const signup = async (req, res) => {
   try {
@@ -31,7 +32,8 @@ export const signup = async (req, res) => {
 
     if (newUser) {
       await newUser.save();
-      //Token 
+      //Token
+      generateTokenAndSetCookie(newUser._id, res);
 
       res.status(201).json({
         _id: newUser._id,
@@ -41,22 +43,47 @@ export const signup = async (req, res) => {
         profilePic: newUser.profilePic,
         message: "User created successfully",
       });
-    }else{
-      res.status(400).json({message: "Invalid user data"})
+    } else {
+      res.status(400).json({ message: "Invalid user data" });
     }
-
   } catch (error) {
     console.log("Error in signup controller ", error.message);
     res.status(500).json({ error: "Internal server error" });
   }
 };
 
-export const login = (req, res) => {
-  console.log(" login user");
-  res.send("login ");
+export const login = async (req, res) => {
+  try {
+    const {username, password} = req.body;
+    
+    const user = await User.findOne({username});
+    const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
+
+    if(!user || !isPasswordCorrect){
+      return res.status(400).json({error: "Invalid username or password"})
+    }
+
+    generateTokenAndSetCookie(user._id, res);
+
+    res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      username: user.username,
+      profilePic: user.profilePic
+    })
+
+  } catch (error) {
+    console.log("Error in login controller ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
 
-export const logout = (req, res) => {
-  console.log("logout user");
-  res.send("logout ");
+export const logout = async(req, res) => {
+  try {
+    res.cookie("mern-chat-app-token", "", {maxAge: 0});
+    res.status(200).json({message: "Logged out successfully"})
+  } catch (error) {
+    console.log("Error in logout controller ", error.message);
+    res.status(500).json({ error: "Internal server error" });
+  }
 };
